5fe1e0cdb57817fe1adb5d563c8b994f532b5d76
winner00 <toverturf@gmail.com>
2013-07-30 22:43:13 -0600

    Build kernel from source.
    Change-Id: Ic591560a6a23d1d2c13ba964fcae26582f45b61f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,6 +26,9 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_NO_BOOTLOADER := false
 
+TARGET_KERNEL_SOURCE := kernel/asus/flo
+TARGET_KERNEL_CONFIG := flo_defconfig
+
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 # BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2

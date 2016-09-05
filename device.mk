a7fade205e29fde5b2e1b9c0a853300367cc75c6
DevSwift <DevSwift@users.noreply.github.com>
2016-09-05 14:03:56 +0200

    OpenGapps support

--- device.mk
+++ device.mk
@@ -32,11 +32,16 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     nfc_nci.bcm2079x.default
+    
+GAPPS_VARIANT := micro
 
 # the actual meat of the device-specific product definition
 $(call inherit-product, device/asus/flo/device-common.mk)
 
 # inherit from the non-open-source side, if present
 $(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
+
+# OpenGapps
+$(call inherit-product, vendor/google/build/opengapps-packages.mk)
 
 DEVICE_PACKAGE_OVERLAYS := device/asus/flo/overlay
